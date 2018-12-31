12131235aec45ce6cdf708252b7c31adda76590b
bLue <tbdblue@gmail.com>
2018-12-31 19:48:50 +0800

    U: bark search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <van-search placeholder="搜索📚" v-model="searchInput" @search="onSearch" @clear="onClear" />
         <van-list finished>
             <van-cell v-for="item in books" :key="item._id"
                       :title="item.title" :label="item.author"
@@ -11,13 +12,14 @@
 </template>
 
 <script>
-    import {getRank} from "../spider";
+    import {getRank, fuzzySearch} from "../spider";
 
     export default {
         name: 'Home',
         data() {
             return {
                 books:[],
+                searchInput: '',
             }
         },
         async mounted() {
@@ -28,6 +30,18 @@
             formatLink(id) {
                 return `/bookInfo/${id}`;
             },
+            async onSearch() {
+                if (!this.searchInput) {
+                    this.onClear();
+                    return;
+                }
+                const res = await fuzzySearch(this.searchInput);
+                this.books = res.books;
+            },
+            async onClear() {
+                const res = await getRank('54d42d92321052167dfb75e3');
+                this.books = res.ranking.books;
+            }
         },
     };
 </script>
